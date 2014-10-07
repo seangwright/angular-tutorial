e56b3f8603b88d2673d6f92bbf0a02b060a21983
Sean Wright <s.galen.wright@gmail.com>
2014-10-06 23:12:12 -0400

    Added ngRepeat and psuedo-form methods

--- js/wv.controllers.demo.js
+++ js/wv.controllers.demo.js
@@ -1,44 +1,79 @@
 (function () {
     'use strict';
-    
+
     var wvControllersModule = angular.module('wv.controllers');
-    
+
     wvControllersModule.controller('demoController', demoController);
-    
+
     demoController.$inject = ['$scope'];
 
     function demoController($scope) {
         var vm = this;
-        
+
         // View Model Properties
-        
-        vm.name = "Elmo";
-        vm.nameTemp = "";
+
+        vm.people = [{
+            id: 1,
+            name: "Elmo",
+            isModified: false
+        }, {
+            id: 2,
+            name: "Jimbo",
+            isModified: false
+        }, {
+            id: 3,
+            name: "Larry Boi",
+            isModified: false
+        }];
+
         vm.input = "";
-        
+        vm.currentId = -1;
+
         // View Model Methods
-        
+
         vm.updateName = updateName;
-        vm.isNameModified = isNameModified;
-        vm.resetName = resetName;
-        
-        function updateName() {
-            if(vm.nameTemp === "") {
-                vm.nameTemp = vm.name;
+        vm.editPerson = editPerson;
+        vm.resetNames = resetNames;
+
+        function updateName(id) {
+            var x, person;
+            for (x = 0; x < vm.people.length; x += 1) {
+                person = vm.people[x];
+                if (person.id === id && vm.input !== person.name) {
+                    if (person.tempName === undefined) {
+                        person.tempName = person.name;
+                    }
+                    person.name = vm.input;
+                    person.isModified = true;
+                    
+                    vm.input = "";
+                    vm.currentId = "";
+                    return;
+                }
             }
-            vm.name = vm.input;
         }
         
-        function isNameModified() {
-            if(vm.nameTemp !== "") {
-                return true;
+        function editPerson(id) {
+            var x, person;
+            for (x = 0; x < vm.people.length; x += 1) {
+                person = vm.people[x];
+                if (person.id === id) {
+                    vm.input = person.name;
+                    vm.currentId = person.id;
+                    return;
+                }
             }
-            return false;
         }
-        
-        function resetName() {
-            vm.name = vm.nameTemp;
-            vm.nameTemp = "";
+
+        function resetNames() {
+            var x, person;
+            for (x = 0; x < vm.people.length; x += 1) {
+                person = vm.people[x];
+                if (person.tempName !== undefined) {
+                    person.name = person.tempName;
+                    person.isModified = false;
+                }
+            }
         }
     }
 }());
